50702e2e0108c81a2b6b6ab68507d28ac42ce6ef
arthurlferrao <arthur.ferrao@unico.io>
2022-05-19 11:00:55 -0300

    Add button for facetec camera

--- public/check-sdk.js
+++ public/check-sdk.js
@@ -3,6 +3,7 @@ import { UnicoCheckBuilder, SelfieCameraTypes, UnicoThemeBuilder, DocumentCamera
 const RESOURCES_DIRECTORY = "/resources"
 const MODELS_PATH = "http://localhost:3000/models"
 
+// Definições de tema para customização do frame de câmera
 const unicoTheme = new UnicoThemeBuilder()
     .setColorSilhouetteSuccess("#0384fc")
     .setColorSilhouetteError("#D50000")
@@ -19,13 +20,18 @@ const unicoTheme = new UnicoThemeBuilder()
     translate(50%, -50%); z-index: 10; text-align: center;">Carregando...</div>`)
     .build();
 
-const unicoCameraBuilder = new UnicoCheckBuilder()
-unicoCameraBuilder.setResourceDirectory(RESOURCES_DIRECTORY)
-unicoCameraBuilder.setModelsPath(MODELS_PATH)
-unicoCameraBuilder.setTheme(unicoTheme)
+/*
+  Criar instancia para câmera.
+  Recursos adicionais são necessários com funcionalidades da Facetec.
+  Arquivos dos modelos de IA necessários para Câmera Inteligente.
+*/
+const unicoCamera = new UnicoCheckBuilder()
+    .setResourceDirectory(RESOURCES_DIRECTORY)
+    .setModelsPath(MODELS_PATH)
+    .setTheme(unicoTheme)
+    .build()
 
-const unicoCamera = unicoCameraBuilder.build()
-
+// Objeto com funções callback para casos de sucesso e erro
 const callback = {
     on: {
         success: function(obj) {
@@ -40,24 +46,27 @@ const callback = {
     }
 }
 
-const startSelfCamera = async (cameraType) => {
-    const { open } = await unicoCamera.prepareSelfieCamera("/services.json", cameraType)
+const startSelfCamera = async (apiKeyPath, cameraType) => {
+    const { open } = await unicoCamera.prepareSelfieCamera(apiKeyPath, cameraType)
     open(callback)
 }
 
-const startDocumentCamera = async (cameraType) => {
-    const { open } = await unicoCamera.prepareDocumentCamera("/services.json", cameraType)
+const startDocumentCamera = async (apiKeyPath, cameraType) => {
+    const { open } = await unicoCamera.prepareDocumentCamera(apiKeyPath, cameraType)
     open(callback)
 }
+
+const facetecCameraEl = document.getElementById("facetec-camera")
+facetecCameraEl.addEventListener("click", () => startSelfCamera("/services.json", SelfieCameraTypes.NORMAL))
 
 const normalCameraEl = document.getElementById("normal-camera")
-normalCameraEl.addEventListener("click", () => startSelfCamera(SelfieCameraTypes.NORMAL))
+normalCameraEl.addEventListener("click", () => startSelfCamera("/services-sem-facetec.json", SelfieCameraTypes.NORMAL))
 
 const smartCameraEl = document.getElementById("smart-camera")
-smartCameraEl.addEventListener("click", () => startSelfCamera(SelfieCameraTypes.SMART))
+smartCameraEl.addEventListener("click", () => startSelfCamera("/services-sem-facetec.json", SelfieCameraTypes.SMART))
 
 const cnhCameraEl = document.getElementById("cnh-camera")
-cnhCameraEl.addEventListener("click", () => startDocumentCamera(DocumentCameraTypes.CNH))
+cnhCameraEl.addEventListener("click", () => startDocumentCamera("/services-sem-facetec.json", DocumentCameraTypes.CNH))
 
 const genericDocumentCameraEl = document.getElementById("generic-document-camera")
-genericDocumentCameraEl.addEventListener("click", () => startDocumentCamera(DocumentCameraTypes.OTHERS("Other")))
+genericDocumentCameraEl.addEventListener("click", () => startDocumentCamera("/services-sem-facetec.json", DocumentCameraTypes.OTHERS("Other")))
